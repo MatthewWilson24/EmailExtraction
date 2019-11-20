3f9e6491b5dcb4e28c9111c88aefd2e0f84b6f22
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-11-20 13:50:28 +0000

    Email Counter refactored with Regex

--- src/com/company/EmailCounter.java
+++ src/com/company/EmailCounter.java
@@ -1,96 +1,27 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailCounter {
 
-    int count;
-    int charNum;
-    String txt;
-    Character currChar;
-    int length;
-    CheckStage stage;
+    private String txt;
 
-    static ArrayList<Character> specialChars = new ArrayList<Character>(Arrays.asList('\'', '_', '%', '+', '-'));
-    static CheckStage[] stageList = CheckStage.values();
+    private static String domain = "[\\w.'%+-]+";
+    private static Pattern pattern = Pattern.compile(" " + domain + "@" + domain + "\\." + domain);
 
     public EmailCounter(String input) {
-        count = 0;
-        charNum = 0;
         txt = input;
-        currChar = txt.charAt(0);
-        length = txt.length();
-        stage = CheckStage.LOCALSTART;
-    }
-
-    private enum CheckStage {
-        LOCALSTART, LOCALEND, SUB1START, SUB1END, SUB2START
     }
 
     public int runCount() {
-        count = 0;
+        int count = 0;
+        Matcher matcher = pattern.matcher(txt);
 
-        while (charNum < length) {
-            currChar = txt.charAt(charNum);
-
-            switch (stage) {
-                case LOCALSTART:
-                    if (isValidChar(currChar)) {
-                        next();
-                    }
-                    break;
-
-                case LOCALEND:
-                    if (currChar.equals('@')) {
-                        next();
-                    }
-                    else if (!isValidChar(currChar)) {
-                        reset();
-                    }
-                    break;
-
-                case SUB1START:
-                    if (isValidChar(currChar)) {
-                        next();
-                    }
-                    else {
-                        reset();
-                    }
-                    break;
-
-                case SUB1END:
-                    if (currChar.equals('.')) {
-                        next();
-                    }
-                    else if (!isValidChar(currChar)) {
-                        reset();
-                    }
-                    break;
-
-                case SUB2START:
-                    if (isValidChar(currChar)) {
-                        count += 1;
-                    }
-                    reset();
-                    break;
-            }
-
-            charNum += 1;
+        while (matcher.find()) {
+            count += 1;
         }
 
         return count;
-    }
-
-    private void next() {
-        stage = stageList[(stage.ordinal()) + 1];
-    }
-
-    private void reset() {
-        stage = CheckStage.LOCALSTART;
-    }
-
-    private static boolean isValidChar(Character c) {
-        return Character.isLetter(c) || Character.isDigit(c) || specialChars.contains(c);
     }
 }
